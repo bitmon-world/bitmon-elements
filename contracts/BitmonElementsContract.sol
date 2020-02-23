3d1a6b6158c4ece6aa4889439806909b71dab981
Enrique <eabz@polispay.org>
2020-02-23 10:49:51 -0600

    Update BitmonElementsContract.sol

--- contracts/BitmonElementsContract.sol
+++ contracts/BitmonElementsContract.sol
@@ -6,6 +6,4 @@ import "@openzeppelin/contracts/token/ERC721/ERC721Burnable.sol";
 
 contract BitmonElementsContract is ERC721Full, ERC721MetadataMintable, ERC721Burnable {
     constructor() ERC721Full("Bitmon Elements", "BME") public {}
-
-
 }
